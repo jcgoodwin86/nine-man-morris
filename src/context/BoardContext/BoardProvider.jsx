6d2c550865e150ec09f047361c4b5804626d289a
jcgoodwin86 <jcgoodwin86@users.noreply.github.com>
2024-07-12 11:57:19 -0400

    Add moving logic

--- src/context/BoardContext/BoardProvider.jsx
+++ src/context/BoardContext/BoardProvider.jsx
@@ -2,7 +2,18 @@
 import React from "react";
 import BoardContext from "./BoardContext";
 import PlayerContext from "../PlayerContext/PlayerContext";
+import PhaseContext from "../PhaseContext/PhaseContext";
 import generateInitialBoard from "../../utils/boardGenerator";
+import run from "../../utils/getMoveLocations";
+
+function moveCheck(currentPiece, moveSpot) {
+  const moves = run();
+  const pieceLocation = currentPiece.slice(0, -2);
+  const moveTo = moveSpot.slice(0, -2);
+  const allowedMoves = moves[pieceLocation];
+
+  return allowedMoves.includes(moveTo);
+}
 
 export default function BoardProvider({ children }) {
   const {
@@ -11,32 +22,45 @@ export default function BoardProvider({ children }) {
     nextPlayerTurn,
     getPieceEle,
     findPiece,
-    removePlayerPiece,
+    movePlayerPiece,
   } = React.useContext(PlayerContext);
-
+  const { phaseState } = React.useContext(PhaseContext);
   const [gameBoard] = React.useState(generateInitialBoard);
+  const [currentPiece, setCurrentPiece] = React.useState();
 
-  const handlePieceClick = React.useCallback(
-    (pieceAtCell) => {
-      removePlayerPiece(pieceAtCell);
-    },
-    [removePlayerPiece],
-  );
+  const handlePieceClick = (pieceAtCell) => {
+    const isCurrentPlayerPiece = pieceAtCell.at(-1) === String(playerTurn);
 
-  const handleCellClick = React.useCallback(
-    (rowIndex, cellIndex) => {
-      if (gameBoard[rowIndex][cellIndex] === "P") {
-        addPlayerPiece(playerTurn, rowIndex, cellIndex);
-        nextPlayerTurn();
+    if (phaseState.phase1.completed && isCurrentPlayerPiece) {
+      if (currentPiece != null) {
+        // This is to deselect current piece
+        setCurrentPiece(() => null);
+      } else {
+        // Select piece user clicked
+        setCurrentPiece(() => pieceAtCell);
       }
-    },
-    [gameBoard, addPlayerPiece, playerTurn, nextPlayerTurn],
-  );
+    }
+  };
+
+  const handleCellClick = (rowIndex, cellIndex) => {
+    if (!phaseState.phase1.completed) {
+      addPlayerPiece(playerTurn, rowIndex, cellIndex);
+      nextPlayerTurn();
+    } else if (currentPiece) {
+      const moveSpot = `${rowIndex},${cellIndex},${playerTurn}`;
+      const canMove = moveCheck(currentPiece, moveSpot);
+      if (canMove) {
+        movePlayerPiece(currentPiece, moveSpot);
+        setCurrentPiece(null);
+      }
+    }
+  };
 
   const value = {
     gameBoard,
     handleCellClick,
-    getPieceEle: (pieceAtCell) => getPieceEle(pieceAtCell, handlePieceClick),
+    getPieceEle: (pieceAtCell) =>
+      getPieceEle(pieceAtCell, handlePieceClick, currentPiece),
     findPiece,
     handlePieceClick,
   };
--- src/context/PhaseContext/PhaseProvider.jsx
+++ src/context/PhaseContext/PhaseProvider.jsx
@@ -5,36 +5,39 @@ const startingPhases = {
   phase1: {
     completed: false,
   },
+  phase2: {
+    completed: false,
+  },
 };
 
-const PhaseReducer = (state, action) => {
+const PhaseReducer = (phaseState, action) => {
   switch (action.type) {
     case "UPDATE_PHASES":
-      // This action payload contains the entire gamePhases state to update
-      return { ...state, ...action.payload };
+      // This action payload contains the entire gamePhases phaseState to update
+      return { ...phaseState, ...action.payload };
     default:
-      return state;
+      return phaseState;
   }
 };
 
 export default function PhaseProvider({ children }) {
-  const [state, dispatch] = React.useReducer(PhaseReducer, startingPhases);
+  const [phaseState, dispatch] = React.useReducer(PhaseReducer, startingPhases);
 
   const phaseOneCheck = React.useCallback(
     (playerPieces) => {
       const isPhaseOneCompleted = playerPieces.size === 18;
-      if (state.phase1.completed !== isPhaseOneCompleted) {
+      if (phaseState.phase1.completed !== isPhaseOneCompleted) {
         dispatch({
           type: "UPDATE_PHASES",
           payload: { phase1: { completed: isPhaseOneCompleted } },
         });
       }
     },
-    [state.phase1.completed], // only re-create this function if `state.phase1.completed` changes
+    [phaseState.phase1.completed],
   );
 
   return (
-    <PhaseContext.Provider value={{ ...state, checkPhaseOne: phaseOneCheck }}>
+    <PhaseContext.Provider value={{ phaseState, checkPhaseOne: phaseOneCheck }}>
       {children}
     </PhaseContext.Provider>
   );
